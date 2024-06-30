4b295dd0dc33d4d1f6eb313bcbfecee44ac2ff2f
CyberHurricane <151047607+CyberHurricane@users.noreply.github.com>
2024-06-30 22:27:06 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+namespace NetworkScanner
+{
+    internal class NetworkScanner
+    {
+        static void Main(string[] args)
+        { 
+            
+        }
+    }
+    public class UdpBroadcast
+    {
+        public static void SendBroadcast(string message, int port)
+        {
+            using (UdpClient UdpClient = new UdpClient())
+            {
+                UdpClient.EnableBroadcast = true;
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Broadcast, port);
+                byte[] bytes = Encoding.UTF8.GetBytes(message);
+                UdpClient.Send(bytes, bytes.Length, endPoint);
+                Console.WriteLine("Broadcast message sent: " + message);
+            }
+        }
+    }
+}
